8d5743dfc0a5455d7f2be447b44a5edc049402bd
sodikovuz <123coder@inbox.ru>
2026-05-08 18:43:00 +0500

    Add files via upload

---
+++ backend/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+};
